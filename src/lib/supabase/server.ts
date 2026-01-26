8938bf041a200eb3baccfa14793ef5b1b6a6e6ab
bagasann <abagas225@gmail.com>
2026-01-27 02:35:34 +0700

    fixing

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -7,7 +7,7 @@ export async function createClient() {
 
     return createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL || "https://example.com",
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "dummy-key",
         {
             cookies: {
                 getAll() {
